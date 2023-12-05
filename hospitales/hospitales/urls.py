52ef7dbab8e78190b750c52942ad4677a07c3858
Mjerrabaine <af.galvis@uniandes.edu.co>
2023-12-05 16:46:28 -0500

    urls

--- hospitales/hospitales/urls.py
+++ hospitales/hospitales/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from django.views.decorators.csrf import csrf_exempt
 
 from . import views
-
+#urls
 urlpatterns = [
     url(r'^hospitales/', views.HospitalList),
     url(r'^hospitalcreate/$', csrf_exempt(views.HospitalCreate), name='hospitalCreate'),
